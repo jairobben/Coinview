51cf9f513c66afb1e012ac844fc0f9333790cb9e
jairobben <93789765+jairobben@users.noreply.github.com>
2021-11-13 19:54:00 +0100

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -22,4 +22,46 @@ for candlestick in candlesticks:
 
 csvfile.close()
 
+
+
+----------------------------------------------------------------------------------------------------------------------------
+
+
+import config, csv
+from binance.client import Client
+from datetime import datetime
+import pandas as pd
+
+client = Client(config.API_KEY, config.API_SECRET)
+
+
+def getminutedata(symbol, interval, lookback):
     
+    frame = pd.DataFrame(client.get_historical_klines(symbol, 
+                                                      interval, 
+                                                      lookback + ' min ago UTC'))
+    frame = frame.iloc[:,:12]
+    frame.columns=['Time', 'Open', 'High', 'Low', 'Close', 'Volume','close_time', 'asset_volume', 'n_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume','ignore']
+    frame = frame.set_index('Time')
+    frame.index = pd.to_datetime(frame.index, unit='ms')
+    frame = frame.astype(float)
+    #candlesticks = client.get_historical_klines(symbol,interval,lookback)
+
+    return frame
+
+
+candlesticks2 = getminutedata('ADAUSDT','1m','100')
+
+for candlestick in candlesticks2:
+    candlestick[0]= candlestick[0]/1000
+    candlestick_writer.writerow(candlestick)
+
+
+print(candlesticks2)
+
+
+csvfile = open('1 minuto.csv', 'w', newline='') 
+candlestick_writer = csv.writer(csvfile, delimiter=',')
+for candlestick in  candlesticks:
+    candlestick_writer.writerow(candlestick)
+csvfile.close()
